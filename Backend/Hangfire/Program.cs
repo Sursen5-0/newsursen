a5172e8b3120d0a133d562061c2538c1deb8a6e3
Bjørn Andersen <bjorn.andersen@twoday.com>
2025-05-13 11:04:47 +0200

    Initial hangfire dashboard add

--- Backend/Hangfire/Program.cs
+++ Backend/Hangfire/Program.cs
@@ -1,6 +1,22 @@
+using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(config =>
+{
+    config.UseInMemoryStorage(); // Use in-memory storage for demo purposes
+});
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Use Hangfire Dashboard
+app.UseHangfireDashboard(); // By default, available at /hangfire
+
+app.MapGet("/", () => "Hangfire Dashboard is available at /hangfire");
 
 app.Run();

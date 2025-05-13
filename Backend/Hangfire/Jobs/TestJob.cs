e86a00d9583fe144e7bd6bea26dc5ed91646d9ad
Bjørn Andersen <bjorn.andersen@twoday.com>
2025-05-13 12:57:47 +0200

    added an example job on a schedule

---
+++ Backend/Hangfire/Jobs/TestJob.cs
@@ -0,0 +1,10 @@
+﻿namespace Hangfire.Jobs
+{
+    public class TestJob
+    {
+        public static void WriteTest()
+        {
+            Console.WriteLine("this is a test");
+        }
+    }
+}
--- Backend/Hangfire/Program.cs
+++ Backend/Hangfire/Program.cs
@@ -1,4 +1,5 @@
 using Hangfire;
+using Hangfire.Jobs;
 using Hangfire.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.EnvironmentVariables;
@@ -11,8 +12,17 @@ builder.Services.AddHangfire(config =>
     config.UseInMemoryStorage(); // Use in-memory storage for demo purposes
 });
 builder.Services.AddHangfireServer();
-
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var jobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
+
+    jobManager.AddOrUpdate(
+        "my-recurring-job",
+        () => TestJob.WriteTest(), // this will fail if you use instance methods like this
+        Cron.Minutely);
+}
 
 app.UseHangfireDashboard(); 
 app.Run();

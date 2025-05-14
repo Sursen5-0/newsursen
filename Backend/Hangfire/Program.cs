77bb846d1fc305c6f4c5a3b8736b40d46a00fe7a
Bjørn Andersen <bjorn.andersen@twoday.com>
2025-05-14 13:20:20 +0200

    added serilog as loggin entity

--- Backend/Hangfire/Program.cs
+++ Backend/Hangfire/Program.cs
@@ -2,12 +2,22 @@ using Application.Secrets;
 using Hangfire;
 using Hangfire.Jobs;
 using Infrastructure.Secrets;
+using Serilog;
+using Serilog.Events;
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddHangfire(config =>
 {
     config.UseInMemoryStorage(); // Use in-memory storage for demo purposes
+    config.UseSerilogLogProvider();
 });
 builder.Services.AddHangfireServer();
 builder.Services.AddHttpClient();
@@ -15,8 +25,8 @@ builder.Services.AddScoped<TestJob>();
 builder.Services.AddScoped<ISecretClient,DopplerClient>(provider =>
 {
     var httpclient = provider.GetRequiredService<HttpClient>();
-    var token = Environment.GetEnvironmentVariable("doppler_key");
-    var environment = Environment.GetEnvironmentVariable("Environment");
+    var token = Environment.GetEnvironmentVariable("doppler_key", EnvironmentVariableTarget.Machine);
+    var environment = Environment.GetEnvironmentVariable("Environment", EnvironmentVariableTarget.Machine);
     return new DopplerClient(httpclient, token, environment);
 });
 builder.Services.AddLogging(loggingbuilder =>
@@ -25,7 +35,7 @@ builder.Services.AddLogging(loggingbuilder =>
     .SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace)
     .AddConsole();
 });
-
+builder.Services.AddSerilog();
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())

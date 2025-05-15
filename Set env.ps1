9b0385e9da8103412ef0203874adc652c8fe7c93
Bjørn <bjand49@gmail.com>
2025-05-15 09:37:17 +0200

    test update

--- Set env.ps1
+++ Set env.ps1
@@ -1,1 +1,3 @@
 [System.Environment]::SetEnvironmentVariable("doppler_key", "dp.st.dev.T7U2yTAldcyiqG5P3BvgCv0LcpJmgCVeA2AzMW4c8v1", "Machine")
+
+[System.Environment]::SetEnvironmentVariable("Environment", "dev", "Machine")

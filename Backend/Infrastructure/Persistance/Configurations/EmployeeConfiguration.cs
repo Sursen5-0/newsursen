460087bf7e70be25ef9a2af60463d0f04d160649
Bjørn <bjand49@gmail.com>
2025-05-21 15:33:43 +0200

    fix

--- Backend/Infrastructure/Persistance/Configurations/EmployeeConfiguration.cs
+++ Backend/Infrastructure/Persistance/Configurations/EmployeeConfiguration.cs
@@ -23,7 +23,6 @@ namespace Infrastructure.Persistance.Configurations
             builder.Property(e => e.PersonalPhoneNumber).HasMaxLength(20);
             builder.Property(e => e.FirstName).HasMaxLength(255);
             builder.Property(e => e.LastName).HasMaxLength(255);
-            builder.Property(e => e.EntraId).HasMaxLength(255).IsFixedLength();
             builder.Property(e => e.FlowCaseId).HasMaxLength(24).IsFixedLength().IsUnicode(false);
             builder.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
             builder.Property(e => e.UpdatedAt).HasDefaultValueSql("(getdate())");

c0eb5fb74b6b6560a3e1cc9e951de057235742ed
Bjørn <bjand49@gmail.com>
2025-05-26 19:40:39 +0200

    added service test

---
+++ Backend/UnitTests/Common/LogVerifier.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Common
+{
+    public static class LogVerifier
+    {
+        public static void VerifyLog<T>(this Mock<ILogger<T>> logger, LogLevel level, Times times)
+        {
+            logger.Verify(x =>
+                x.Log(level,
+                      It.IsAny<EventId>(),
+                      It.Is<It.IsAnyType>((_, __) => true),
+                      It.IsAny<Exception>(),
+                      It.Is<Func<It.IsAnyType, Exception, string>>((_, __) => true)),
+                times);
+        }
+    }
+}

f4a84f78088d4057aede171a3f0bcc4197bc5bf8
Troels <91475923+Troels-dat21B@users.noreply.github.com>
2025-05-26 17:07:26 +0200

    Sur 31 (#20)
    * Added integrations test for Flowcase connection. Created Flowcase files
    
    * update to yaml and environment variables
    
    * yupdated integration folder location
    
    ---------
    
    Co-authored-by: Troels-dat21B <troe1912@dat.stud.kea>
    Co-authored-by: Bjørn <bjand49@gmail.com>

---
+++ Backend/IntegrationsTests/FlowCaseIntegrationsTests.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Interfaces.ExternalClients;
+using Infrastructure.FlowCase;
+using Infrastructure.Secrets;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationsTests;
+
+public class FlowCaseIntegrationsTests
+{
+    private readonly FlowCaseClient _client;
+    private readonly DopplerClient _secretClient;
+
+    public FlowCaseIntegrationsTests()
+    {
+        var token = Environment.GetEnvironmentVariable("DOPPLER_KEY");
+        var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+        var secretHttpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://api.doppler.com/v3/"),
+            DefaultRequestHeaders = { Authorization = new AuthenticationHeaderValue("Bearer", token) }
+        };
+        _secretClient = new DopplerClient(secretHttpClient, token, environment);
+        var flowCaseHttpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://twoday.flowcase.com")
+        };
+
+        _client = new FlowCaseClient(_secretClient, flowCaseHttpClient);
+    }
+
+    [Fact]
+    public async Task TestFlowCaseClient()
+    {
+        // Arrange
+        var email = "bjorn.andersen@twoday.com";
+
+        // Act
+        var response = await _client.GetUser(email); //Used a random used endpoint for testing
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+}

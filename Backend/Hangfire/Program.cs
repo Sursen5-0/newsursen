11f3901879676f1415e263679d24edad33dafdfd
Bjand49 <bjand49@gmail.com>
2025-06-03 13:34:14 +0200

    Update to hangfire jobs and last updated (#34)

--- Backend/Hangfire/Program.cs
+++ Backend/Hangfire/Program.cs
@@ -15,6 +15,7 @@ using Serilog;
 using Serilog.Events;
 using System.Net.Http.Headers;
 using Infrastructure.Common;
+using Microsoft.Extensions.Configuration;
 
 var token = Environment.GetEnvironmentVariable("DOPPLER_KEY");
 var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
@@ -55,16 +56,15 @@ builder.Services.AddSerilog();
 
 builder.Services.AddHangfire(config =>
 {
-    config.UseInMemoryStorage();
     config.UseSerilogLogProvider();
 });
 builder.Services.AddHangfireServer();
 
 builder.Services.AddHttpClient();
-builder.Services.AddScoped<TestJob>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IJobExecutionRepository, JobExecutionRepository>();
 builder.Services.AddScoped<SeveraJobs>();
 builder.Services.AddScoped<EntraJobs>();
 builder.Services.AddDbContext<SursenContext>((services, options) =>
@@ -82,7 +82,7 @@ builder.Services.AddScoped<ISecretClient, DopplerClient>(provider =>
     httpClient.BaseAddress = new Uri("https://api.doppler.com/v3/");
     return new DopplerClient(httpClient, token, environment);
 });
-builder.Services.AddHttpClient<ISeveraClient,SeveraClient>(client =>
+builder.Services.AddHttpClient<ISeveraClient, SeveraClient>(client =>
 {
     client.BaseAddress = new Uri("https://api.severa.visma.com/rest-api/v1.0/");
 })
@@ -104,14 +104,17 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var jobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
+    var secretClient = scope.ServiceProvider.GetRequiredService<ISecretClient>();
+    var hangfireConnection = secretClient.GetSecretAsync("HANGFIRE_CONNECTIONSTRING").Result;
 
-    var testJob = scope.ServiceProvider.GetRequiredService<TestJob>();
-    jobManager.AddOrUpdate("my-recurring-job", () => testJob.WriteTest(), Cron.Minutely);
+    GlobalConfiguration.Configuration
+        .UseSqlServerStorage(hangfireConnection)
+        .UseSerilogLogProvider();
+    var jobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
 
     jobManager.AddOrUpdate(
         "SynchronizeEmployees",
-        () => scope.ServiceProvider.GetRequiredService<SeveraJobs>().SynchronizeEmployees(),"0 0 31 2 *");
+        () => scope.ServiceProvider.GetRequiredService<SeveraJobs>().SynchronizeEmployees(), "0 0 31 2 *");
     jobManager.AddOrUpdate(
         "SynchronizeContracts",
         () => scope.ServiceProvider.GetRequiredService<SeveraJobs>().SynchronizeContracts(), "0 0 31 2 *");
---
+++ Backend/Infrastructure/Persistance/Repositories/JobExecutionRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Infrastructure.Persistance.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.Repositories
+{
+    public class JobExecutionRepository(SursenContext _db) : IJobExecutionRepository
+    {
+        public async Task<DateTime?> GetLatestSuccessfulJobExecutionByName(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                return null;
+            }
+            var jobExecution = await _db.JobExecutions.Where(x => x.JobName == name && x.IsSuccess)
+                .OrderByDescending(x => x.CompletionDate)
+                .FirstOrDefaultAsync();
+
+            if(jobExecution == null)
+            {
+                return null;
+            }
+            else
+            {
+                return jobExecution.CompletionDate;
+            }
+
+        }
+
+        public async Task InsertJobExecution(JobExecutionDTO job)
+        {
+            var jobExecution = new JobExecution
+            {
+                Id = Guid.NewGuid(),
+                JobName = job.JobName,
+                StartTimeDate = job.StartTimeDate,
+                CompletionDate = job.CompletionDate,
+                ErrorMessage = job.ErrorMessage,
+                IsSuccess = job.IsSuccess
+            };
+            _db.JobExecutions.Add(jobExecution);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
